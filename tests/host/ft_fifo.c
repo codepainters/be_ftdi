3af3392d0784de844de96d593aab7ee7287e9c8f
Przemyslaw Wegrzyn <pwegrzyn@codepainters.com>
2016-09-14 12:14:43 +0200

    ft_fifo: all the settings required by FTDI sync mode

--- tests/host/ft_fifo.c
+++ tests/host/ft_fifo.c
@@ -14,24 +14,52 @@ struct ftdi_context* ft_fifo_open(int vid, int pid)
 
     ftdi = ftdi_new();
     if (!ftdi) {
-        fprintf(stderr, "Cannot initialize libftdi\n");
+        fprintf(stderr, "libftdi: cannot initialize!\n");
         exit(EXIT_FAILURE);
     }
 
     ret = ftdi_usb_open(ftdi, vid, pid);
     if (ret < 0) {
-        fprintf(stderr, "cannot open FTDI device: %d (%s)\n", ret, ftdi_get_error_string(ftdi));
+        fprintf(stderr, "libftdi: cannot open FTDI device: %d (%s)\n", ret, ftdi_get_error_string(ftdi));
         ftdi_free(ftdi);
         exit(EXIT_FAILURE);
     }
 
     ret = ftdi_set_bitmode(ftdi, 0xFF, BITMODE_SYNCFF);
     if (ret != 0) {
-        fprintf(stderr, "Failed to set mode to Synchronous FIFO");
+        fprintf(stderr, "libftdi: failed to set mode to Synchronous FIFO");
+        ftdi_free(ftdi);
+        exit(EXIT_FAILURE);
+    }
+    
+    ret = ftdi_set_latency_timer(ftdi, 2);
+    if (ret != 0) {
+        fprintf(stderr, "libftdi: failed to set latency timer");
         ftdi_free(ftdi);
         exit(EXIT_FAILURE);
     }
 
+    ret = ftdi_setflowctrl(ftdi, SIO_RTS_CTS_HS);
+    if (ret != 0) {
+        fprintf(stderr, "libftdi: failed to set flow control");
+        ftdi_free(ftdi);
+        exit(EXIT_FAILURE);
+    }
+
+    ret = ftdi_read_data_set_chunksize(ftdi, 0x10000);
+    if (ret != 0) {
+        fprintf(stderr, "libftdi: failed to set read chunk size");
+        ftdi_free(ftdi);
+        exit(EXIT_FAILURE);
+    }
+
+    ret = ftdi_write_data_set_chunksize(ftdi, 0x10000);
+    if (ret != 0) {
+        fprintf(stderr, "libftdi: failed to set write chunk size");
+        ftdi_free(ftdi);
+        exit(EXIT_FAILURE);
+    }
+    
     return ftdi;
 }
 
